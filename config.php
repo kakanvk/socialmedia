ce3b6a509e5faaac37f841d05cd74a000d9b6662
Ka Ka <caoka5265@gmail.com>
2023-04-26 21:32:32 +0700

    First upload

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.wowonder.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com   
+// +------------------------------------------------------------------------+
+// | WoWonder - The Ultimate PHP Social Networking Platform
+// | Copyright (c) 2016 WoWonder. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "root";
+// MySQL Database Password
+$sql_db_pass = "";
+// MySQL Database Name
+$sql_db_name = "social";
+
+// Site URL
+$site_url = "http://localhost/facebook2"; // e.g (http://example.com)
+
+// Purchase code
+$purchase_code = "fo"; // Your purchase code, don't give it to anyone. 
+?>
